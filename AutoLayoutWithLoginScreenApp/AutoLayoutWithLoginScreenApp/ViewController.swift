e8eb4397e0926c6add8121c04f05a801537be41b
Shailendra231 <44691122+Shailendra231@users.noreply.github.com>
2020-02-21 17:32:24 +0530

    Add files via upload

---
+++ AutoLayoutWithLoginScreenApp/AutoLayoutWithLoginScreenApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginScreenApp
+//
+//  Created by Shailendra Kumar Ram  on 2/3/20.
+//  Copyright © 2020 Nitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
